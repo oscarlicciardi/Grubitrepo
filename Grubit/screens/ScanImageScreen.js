3d860909e2af530a8d8dbee4e3c4225bf2cf56a8
gianluL <141229539+gianluL@users.noreply.github.com>
2023-11-10 23:55:27 +0100

    Add files via upload

--- Grubit/screens/ScanImageScreen.js
+++ Grubit/screens/ScanImageScreen.js
@@ -1,12 +1,79 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-const ScanImageScreen = () => {
+
+
+import type { PropsWithChildren } from 'react';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+import React, { useState, useEffect } from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { Button, StyleSheet, Text, TouchableOpacity, View, ScrollView, useColorScheme, LogBox } from 'react-native';
+
+
+
+
+function ScanImageScreen(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scannedCode, setScannedCode] = useState('');
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
   return (
-    <View >
-      <Text>This is the scan image screen</Text>
-    </View>
+
+    <ScrollView>
+      <Text style={{ alignSelf: 'center', color: isDarkMode ? '#FFF' : '#000', margin: 10, marginTop: 50 }}>{'\n'}Take a picture</Text>
+
+      <Camera
+        style={{ width: '100%', height: '200%' }}
+        type={type}>
+      </Camera>
+
+      <TouchableOpacity
+        style={{ margin: 10, marginTop: 50, marginBottom: 30, padding: 15, borderRadius: 15, backgroundColor: '#007AFF', width: 150, alignSelf: 'center', }}>
+        <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white' }}>Add</Text>
+      </TouchableOpacity>
+
+    </ScrollView >
+
   );
-};
+}
 
 export default ScanImageScreen;
+
+/*
+
+import React from 'react';
+import AppNavigator from './navigator/AppNavigator';
+
+const App = () => {
+  return <AppNavigator />;
+};
+
+export default App;
+
+*/
